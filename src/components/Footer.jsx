102ba175002068dc5106b78d274d32fb8875b5ec
jay9949 <jay37192@gmail.com>
2023-12-21 18:16:37 +0530

    #

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,10 @@ const Footer = () => (
 
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
         {footerLinks.map((footerlink) => (
-          <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
+          <div
+            key={footerlink.title}
+            className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}
+          >
             <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
               {footerlink.title}
             </h4>
@@ -30,7 +33,9 @@ const Footer = () => (
                     index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
                   }`}
                 >
-                  {link.name}
+                  <a href={link.link} target="blank">
+                    {link.name}
+                  </a>
                 </li>
               ))}
             </ul>
